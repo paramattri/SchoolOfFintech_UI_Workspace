7170f6fee256d9e37e30c1adc0cbb79aadfd4c39
Param Attri <attri.param@gmail.com>
2020-11-18 19:31:11 +0530

    Initial commit

--- day3/localStorage.js
+++ day3/localStorage.js
@@ -1,61 +1,69 @@
-class Course {
-    constructor(courseId, courseName, fees) {
-        this.courseId = courseId;
-        this.courseName = courseName;
-        this.fees = fees;
-    }
-
-    getCourseDetails() {
-        console.log(this.courseId + "" + this.courseName + "" + this.fees)
-    }
-
-}
-
-class CourseHTM {
-    static addCourse(course, courselist) {
-        const form = document.getElementById('form1');
-        //const courselist = document.createElement('ul');
-        courselist.innerHTML = `<li>${course.courseId}</li>`
-        const submit = document.getElementById('submit');
-        form.appendChild(courselist, submit)
+class Course{
+    constructor(courseId, courseName, fees){
+        this.courseId = courseId
+        this.courseName = courseName
+        this.fees = fees
     }
 }
 
-class LocalStore {
+class CourseHTML{
+    static addCourse(course){
+        const form = document.getElementById('form1')
+        const courseList = document.createElement('ul')
+        courseList.innerHTML = `<li>${course.courseId}<input type='button' value='delete'></li>`
+        const submit = document.getElementById('submit')
+        form.appendChild(courseList, submit)
+    }
+}
 
-    static courses = new Array();
+class LocalStore{
 
-    static addtoLocal(course) {
-
-        //let courses = this.getCourses();
+    static addToLocal(course){
+        let courses = this.getCourses()
         courses.push(course)
         localStorage.setItem('courses', JSON.stringify(courses))
-        //CourseHTM.addCourse(course);
-        // localStorage.removeItem('name')
-        //console.log(course);
-
     }
 
-    static getCourses() {
-        let courses = JSON.parse(localStorage.getItem('courses'));
+    static getCourses(){
+        let courses = JSON.parse(localStorage.getItem('courses'))
+        if(courses == null){
+            courses = []
+        }
         return courses;
     }
-
 }
+
+function loadCourses(){
+    const courses = LocalStore.getCourses()
+    courses.forEach(element => {
+        CourseHTML.addCourse(element)
+    });
+}
+
+//loadCourses()
+document.getElementById('DOMContentLoaded', loadCourses())
+
 document.getElementById("submit").addEventListener('click', (e) => {
+    e.preventDefault()
     const cid = document.getElementById('cid').value;
     const cname = document.getElementById('cname').value;
     const fees = document.getElementById('fees').value;
 
-    const course = new Course(cid, cname, fees);
+    const course = new Course(cid, cname, fees)
+    LocalStore.addToLocal(course)
+    location.reload()
+    // let courses = LocalStore.getCourses()
+    // console.log(courses)
+})
 
-    //const coursehtm = new courseHTM();
-    //coursehtm.addtoLocal(course);
-
-    LocalStore.addtoLocal(course);
-
-    const courselist = document.createElement('ul');
-    let courses = LocalStore.getCourses();
-    console.log(courses)
-    e.preventDefault();
+document.getElementById('form1').addEventListener('click', (e) => {
+    //console.log(e.target.parentElement.textContent)
+    let courses = LocalStore.getCourses()
+    for(let i = 0; i < courses.length; i++){
+        if(courses[i].courseId == e.target.parentElement.textContent){
+            courses.splice(i, 1)
+            location.reload()
+        }
+    }
+    localStorage.setItem('courses', JSON.stringify(courses))
 })
